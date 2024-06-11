ce3b7b11238cfa0fc53f9415201c0354b566c750
Prakhar Bhandari <prakharb2k6@gmail.com>
2024-06-11 12:07:22 +0530

    initialize

---
+++ Cogs/admin.py
@@ -0,0 +1,164 @@
+from contextlib import redirect_stdout
+from io import StringIO
+from os import remove
+from textwrap import indent
+from traceback import format_exc
+from typing import Literal, Optional
+
+import discord
+from discord import File, Object
+from discord.errors import HTTPException
+from discord.ext.commands import Bot, Cog, Context, ExtensionError, Greedy, command, group, is_owner
+
+class Mod(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+        self._last_result = None
+
+
+    @command(hidden=True)
+    @is_owner()
+    async def load(self, ctx: Context, *, module: str) -> None:
+        """Loads a module."""
+        try:
+            await self.bot.load_extension(module)
+
+        except ExtensionError as e:
+            await ctx.send(f"{e.__class__.__name__}: {e}")
+
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
+
+    @command(hidden=True)
+    @is_owner()
+    async def unload(self, ctx: Context, *, module: str) -> None:
+        """Unloads a module."""
+        try:
+            await self.bot.unload_extension(module)
+
+        except ExtensionError as e:
+            await ctx.send(f"{e.__class__.__name__}: {e}")
+
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
+
+    @group(name="reload", hidden=True, invoke_without_command=True)
+    @is_owner()
+    async def _reload(self, ctx: Context, *, module: str) -> None:
+        """Reloads a module."""
+        try:
+            await self.bot.reload_extension(module)
+
+        except ExtensionError as e:
+            await ctx.send(f"{e.__class__.__name__}: {e}")
+
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
+
+    @command(name="sync2", hidden=False)
+    @is_owner()
+    async def _sync(self, ctx: Context, guilds: Greedy[Object], spec: Optional[Literal["~", "*"]] = None) -> None:
+        """Syncs the bot with the guilds.
+        ,sync -> global sync
+        ,sync ~ -> sync current guild
+        ,sync * -> copies all global app commands to current guild and syncs
+        ,sync id_1 id_2 -> syncs guilds with id 1 and 2"""
+        if not guilds:
+            if spec == "~":
+                fmt = await self.bot.tree.sync(guild=ctx.guild)
+
+            elif spec == "*":
+                self.bot.tree.copy_global_to(guild=ctx.guild)
+                fmt = await self.bot.tree.sync(guild=ctx.guild)
+
+            else:
+                fmt = await self.bot.tree.sync()
+
+            await ctx.send(f"Synced {len(fmt)} commands {'globally' if spec is None else 'to the current guild.'}")
+            return
+
+        fmt = 0
+        for guild in guilds:
+            try:
+                await self.bot.tree.sync(guild=guild)
+            except HTTPException:
+                pass
+            else:
+                fmt += 1
+
+        await ctx.send(f"Synced the tree to {fmt}/{len(guilds)} guilds.")
+
+    @staticmethod
+    def cleanup_code(content: str) -> str:
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        content = content.replace("```", "\n```")
+        content = content.replace("```python", "\n```")
+        content = content.replace("```py", "\n```")
+        if content.startswith("```") and content.endswith("```"):
+            return "\n".join(content.split("\n")[1:-1])
+
+        return content.strip("` \n")
+
+    @command(name="eval", aliases=["e"])
+    @is_owner()
+    async def _eval(self, ctx: Context, *, body: str) -> None:
+        """Evaluates a code"""
+        env = {
+            "client": self.bot,
+            "ctx": ctx,
+            "channel": ctx.channel,
+            "author": ctx.author,
+            "guild": ctx.guild,
+            "message": ctx.message,
+            "_": self._last_result,
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = StringIO()
+
+        to_compile = f'async def func():\n{indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f"```py\n{e.__class__.__name__}: {e}\n```")
+
+        func = env["func"]
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f"```py\n{value}{format_exc()}\n```")
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction("\u2705")
+            except:
+                pass
+
+            try:
+                if ret is None:
+                    if value:
+                        await ctx.send(f"```py\n{value}\n```")
+                else:
+                    self._last_result = ret
+                    await ctx.send(f"```py\n{value}{ret}\n```")
+            except HTTPException:
+                with open("result.txt", "w") as f:
+                    f.write(f"{value}{ret}")
+                remove("result.txt")
+
+    @command(hidden=True, name="close", aliases=["shutdown"])
+    @is_owner()
+    async def _close(self, ctx: Context) -> None:
+        """Closes the bot."""
+        await ctx.send("Closing...")
+        await self.bot.close()
+
+
+async def setup(bot):
+    await bot.add_cog(Mod(bot))
---
+++ main.py
@@ -0,0 +1,241 @@
+import discord
+from discord.ext import commands
+import json
+import os
+import aiohttp
+import datetime
+import logging
+from typing import Literal, Optional
+from discord import Object
+from discord.ext.commands import Context, Greedy, Bot
+from discord.errors import HTTPException
+from discord import app_commands
+import keep_alive
+
+with open("E:\\Programming\\bots\\synanit2.0\\secrets.json") as f:
+    secrets = json.load(f)
+
+
+class Synanit(Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(
+            command_prefix="?s", intents=intents, application_id=980733466968748122
+        )
+        # self.initial_extensions = ["Cogs.Mod", "Cogs.Anime", "Cogs.Manga", "Cogs.Character"]
+        self.initial_extensions = ["Cogs.slash", "Cogs.admin"]
+
+    async def setup_hook(self):
+        self.session = aiohttp.ClientSession()
+        # self.pool = AsyncConnectionPool(conninfo=environ["DATABASE_URL"])
+        for ext in self.initial_extensions:
+            await self.load_extension(ext)
+
+    async def close(self):
+        await self.session.close()
+        await super().close()
+
+    async def on_command_error(self, ctx, error):
+        error = str(error).replace("`", "\\`")
+        await ctx.send(f"An error occurred: ```{error}```")
+
+    async def on_ready(self):
+        print("Ready!")
+        MY_GUILD = Object(id=697493731611508737)
+        await self.tree.sync(guild=MY_GUILD)
+
+
+client = Synanit()
+
+
+@client.event
+async def on_ready():
+    print("Ready!")
+
+
+async def load():
+    for filename in os.listdir("./Cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"Cogs.{filename[:-3]}")
+
+
+@client.event
+async def on_presence_update(before, after):
+    if after.status != before.status:
+        print(
+            f"Status change: {before.name} went from {before.status} to {after.status} at {datetime.datetime.now()}"
+        )
+        with open("./userlog.txt", "r") as f:
+            data = f.readlines()
+        data.append(f"{after.name} is now {after.status}\n")
+        with open("./userlog.txt", "w") as f:
+            f.writelines(data)
+        print(after)
+    # if before.activity != after.activity:
+    #     if before.status == discord.Status.offline:
+    #         with open("./userlog.txt", "a") as f:
+    #             f.write(f"{after.name} is now online\n")
+    #     elif after.status == discord.Status.offline:
+    #         with open("./userlog.txt", "a") as f:
+    #             f.write(f"{after.name} is now offline\n")
+
+
+@client.command()
+async def test(ctx):
+    await ctx.send("the bot seems to be working as of rn")
+
+
+@client.command(name="sync")
+async def _sync(
+    ctx: Context, guilds: Greedy[Object], spec: Optional[Literal["~", "*"]] = None
+) -> None:
+    """Syncs the bot with the guilds.
+    ?ssync -> global sync
+    ?ssync ~ -> sync current guild
+    ?ssync * -> copies all global app commands to current guild and syncs
+    ?ssync id_1 id_2 -> syncs guilds with id 1 and 2"""
+    if not guilds:
+        if spec == "~":
+            fmt = await client.tree.sync(guild=ctx.guild)
+
+        elif spec == "*":
+            client.tree.copy_global_to(guild=ctx.guild)
+            fmt = await client.tree.sync(guild=ctx.guild)
+
+        else:
+            fmt = await client.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(fmt)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    fmt = 0
+    for guild in guilds:
+        try:
+            await client.tree.sync(guild=guild)
+        except HTTPException:
+            pass
+        else:
+            fmt += 1
+
+    await ctx.send(f"Synced the tree to {fmt}/{len(guilds)} guilds.")
+
+
+# @client.tree.command(guild=Object(id=890890610339373106))
+@client.tree.command()
+async def yo(interaction: discord.Interaction, name: str):
+    await interaction.response.send_message(f"Yo {name}", ephemeral=True)
+
+
+# make it join a vc on command
+@client.tree.command()
+async def join(
+    interaction: discord.Interaction,
+    id: str,
+    message_append: str = None,
+    mute: bool = False,
+    deafen: bool = False,
+    say_hello: bool = False,
+):
+    id = int(id)
+    await interaction.response.defer()
+    voice_channel = client.get_channel(id)
+    await voice_channel.connect()
+    await interaction.response.send_message(
+        f"Joined {voice_channel.name} {message_append}", ephemeral=False
+    )
+    if mute:
+        await client.voice_clients[0].main_mute()
+    if deafen:
+        await client.voice_clients[0].main_deafen()
+
+
+import requests
+import random
+import interactions
+
+
+@client.tree.command()
+async def qplz(interaction: discord.Interaction, tag: str = "", rating: int = 1400):
+    await interaction.response.defer()
+    colours = [0xDC143C, 0xD35400, 0x48C9B0, 0x7FB3D5]
+    color = random.choice(colours)
+    url = "https://codeforces.com/api/problemset.problems?tags=implementation"
+    # json
+    response = requests.get(url)
+    data = response.json()
+    problems = data["result"]["problems"]
+    tagsReq = []
+    tagsNotReq = []
+    # split tags into words
+    tags = tag.split(", ")
+    for i in tags:
+        if len(i) > 0:
+            if i[0] == "-":
+                tagsNotReq.append(i[1:].lower())
+            else:
+                tagsReq.append(i[1:].lower())
+
+    problemSet = []
+    for problem in problems:
+        if "contestId" not in problem:
+            continue
+        if problem["contestId"] < 1286:
+            continue
+        problem["tags"] = [tag.lower() for tag in problem["tags"]]
+        # check if there's a rating in problem
+        if "rating" not in problem:
+            continue
+        if problem["rating"] == rating:
+            if all(tag.lower() in problem["tags"] for tag in tagsReq) and all(
+                tag.lower() not in problem["tags"] for tag in tagsNotReq
+            ):
+                problemSet.append(problem)
+            # problemSet.append(problem)
+    if problemSet:
+        problem = random.choice(problemSet)
+        problemName = f'{problem["index"]}. {problem["name"]}'
+        problemLink = f"[{problemName}](<https://codeforces.com/contest/{problem['contestId']}/problem/{problem['index']}>)"
+        embeds = interactions.Embed(
+            title=problemName,
+            description=f"Rating: `{problem['rating']}`",
+            url=f"https://codeforces.com/contest/{problem['contestId']}/problem/{problem['index']}",
+            color=color,
+        )
+        # embeds.add_field(name="Rating", value=f"`{problem['rating']}`", inline=False)
+        embeds.add_field(
+            name="Tags", value=f"`{', '.join(problem['tags'])}`", inline=False
+        )
+
+        await interaction.followup.send(
+            # f"Problem: {problemLink}\nRating: {problem['rating']}\nTags: {", ".join(problem['tags'])}\n",
+            embeds=[embeds],
+            ephemeral=False,
+        )
+    else:
+        await interaction.followup.send("No problems found", ephemeral=False)
+
+
+@client.tree.command()
+async def invite(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        "https://discord.com/oauth2/authorize?client_id=980733466968748122&permissions=18015352965184&integration_type=1&scope=applications.commands+bot",
+        ephemeral=False,
+    )
+
+
+# logger = logging.getLogger('discord')
+# logger.setLevel(logging.DEBUG)
+# logging.getLogger('discord.http').setLevel(logging.INFO)
+# handler = logging.StreamHandler()
+# dt_fmt = '%Y-%m-%d %H:%M:%S'
+# formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+
+handler = logging.StreamHandler()
+
+keep_alive.keep_alive()
+
+client.run(secrets["TOKEN"])
